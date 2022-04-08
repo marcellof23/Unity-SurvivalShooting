92f7b8bc6423a2b73c65b6f69b2f165851a35440
DenilsenAxel <denilsenaxel@gmail.com>
2022-04-08 22:39:18 +0700

    feat: add name change in main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,32 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject inputField; 
 
-  public void PlayWef()
-  {
-    SceneManager.LoadScene("Wave_Mode");
-  }
+    void Awake()
+    {
+        PlayerPrefs.SetString("name", "");
+        inputField.SetActive(false);
+    }
 
-  public void PlayZen()
-  {
-    SceneManager.LoadScene("Zen_Mode");
-  }
+    public void ChangeName()
+    {
+        string text = inputField.GetComponent<TMP_InputField>().text;
+        PlayerPrefs.SetString("name", text);
+        string name = PlayerPrefs.GetString("name");
 
-  public void OpenLeaderBoard()
-  {
-    SceneManager.LoadScene("Scoreboard");
-  }
+        inputField.SetActive(false);
+        gameObject.SetActive(true);
+    }
 
-  public void Return()
-  {
-    SceneManager.LoadScene("Main_Menu");
-  }
+    public void ShowInputName()
+    {
+        gameObject.SetActive(false);
+        inputField.SetActive(true);
+    }
 
-  public void QuitGame()
-  {
-    Application.Quit();
-  }
+    public void PlayWef()
+    {
+        SceneManager.LoadScene("Wave_Mode");
+    }
+
+    public void PlayZen()
+    {
+        SceneManager.LoadScene("Zen_Mode");
+    }
+
+    public void OpenLeaderBoard()
+    {
+        SceneManager.LoadScene("Scoreboard");
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
